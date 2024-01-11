43da4efa612cc8b91c44a86834c3ff0140ff91b5
alejandrols22 <alejandrolopsep@gmail.com>
2024-01-11 11:47:39 +0100

    clase 11/01/24

---
+++ 2Trimestre/clase_11_01_23_acceso_datos_parte2/src/main/java/modelo/Main.java
@@ -0,0 +1,57 @@
+package modelo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.context.internal.ThreadLocalSessionContext;
+import org.hibernate.engine.spi.SessionFactoryImplementor;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		// Configurar la sesion de Hibernate
+		SessionFactory sessionFactory = (SessionFactory) new Configuration()
+				.configure()
+				
+				// .configure("hibernate.cfg.xml") 
+				// Ruta del archivo de configuracion de Hibernate
+				.buildSessionFactory(); // Construit la sesion del Hibernate
+		
+		// Configurar la sesion en el contexto actual
+		ThreadLocalSessionContext context = new
+	ThreadLocalSessionContext((SessionFactoryImplementor) sessionFactory);
+		context.bind(sessionFactory.openSession());
+		
+		try {
+			// Crear objeto cliente
+			Clientes cliente = new Clientes("Eva", "Sevilla");
+			
+			
+			// Obtener la sesion actual
+			Session session = context.currentSession();
+			
+			// Iniciar transaccion
+			session.beginTransaction();
+			
+			// Guardar objeto en la base de datos
+			session.save(cliente);
+			
+			// Hacer commit de la transaccion
+			session.getTransaction().commit();
+			
+			// Imprimir cliente guardado en la base de datos
+			System.out.println("Cliente guardado + " + cliente);
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// Desligar la sesion del contexto
+			ThreadLocalSessionContext.unbind(sessionFactory);
+			// Cerrar la sesion de Hibernate
+			sessionFactory.close();
+		}
+	}
+
+}
