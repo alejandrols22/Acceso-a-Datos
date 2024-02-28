c587388c3bd565121c85349675335d00ef7cbcc0
alejandrols22 <alejandrolopsep@gmail.com>
2024-02-28 21:37:59 +0100

    a

---
+++ 2Trimestre/simulacro_accesodatos_2trimestre/src/main/java/ejercicio3/QueryXML.java
@@ -0,0 +1,84 @@
+package ejercicio3;
+
+import javax.xml.transform.OutputKeys;
+
+import org.exist.xmldb.EXistResource;
+import org.xmldb.api.DatabaseManager;
+import org.xmldb.api.base.Collection;
+import org.xmldb.api.base.Database;
+import org.xmldb.api.base.Resource;
+import org.xmldb.api.base.ResourceIterator;
+import org.xmldb.api.base.ResourceSet;
+import org.xmldb.api.base.XMLDBException;
+import org.xmldb.api.modules.XMLResource;
+import org.xmldb.api.modules.XPathQueryService;
+import org.xmldb.api.modules.XQueryService;
+
+
+
+public class QueryXML {
+
+    // URI para conectarse a la base de datos XML existente en localhost en el puerto 8080
+    private static String URI = "xmldb:exist://localhost:8080/exist/xmlrpc/db/";
+
+    // Nombre de la colección donde se encuentran los datos XML
+    private static String COLLECTION = "serie";
+
+    // Nombre del recurso XML dentro de la colección
+    private static String RESOURCE = "serie.xml";
+
+    public static void main(String args[]) throws Exception {
+
+        final String driver = "org.exist.xmldb.DatabaseImpl";
+
+        // Inicializa el controlador de la base de datos
+        Class cl = Class.forName(driver);
+        Database database = (Database) cl.getDeclaredConstructor().newInstance();
+        database.setProperty("create-database", "true");
+        DatabaseManager.registerDatabase(database);
+
+        Collection col = null;
+		XMLResource res = null;
+		try {
+			// Obtiene la colección
+			col = DatabaseManager.getCollection(URI + COLLECTION, "admin", "1234");
+
+			// Configura la propiedad de salida para indentación
+			col.setProperty(OutputKeys.INDENT, "no");
+
+			// Obtiene el recurso XML de la colección
+			res = (XMLResource) col.getResource(RESOURCE);
+
+			// Verifica si el recurso existe
+			if (res == null) {
+				System.out.println("Base de datos no encontrada!");
+			} else {
+				// Imprime el contenido del recurso
+				System.out.println(res.getContent());
+			}
+		} finally {
+			// Limpieza de recursos
+
+			if (res != null) {
+				try {
+					// Libera los recursos del recurso exist
+					((EXistResource) res).freeResources();
+				} catch (XMLDBException xe) {
+					xe.printStackTrace();
+				}
+			}
+
+			
+				if (col == null) {
+				    System.out.println("No se pudo conectar a la colección: " + COLLECTION);
+				    return;
+				}
+				try {
+					// Cierra la colección
+					col.close();
+				} catch (XMLDBException xe) {
+					xe.printStackTrace();
+				}
+			}
+		}
+	}
--- 2Trimestre/simulacro_accesodatos_2trimestre/src/main/java/modelo/GestorProfesores.java
+++ 2Trimestre/simulacro_accesodatos_2trimestre/src/main/java/modelo/GestorProfesores.java
@@ -1,31 +1,34 @@
 package modelo;
 
-
 import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
+
+import modelo.Profesor; 
+
 public class GestorProfesores {
-    private static SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-    
-    public void altaProfesor(Profesor profesor) {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        session.save(profesor);
-        session.getTransaction().commit();
-        session.close();
-    }
-    
-    public void consultarProfesores() {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        List<Profesor> result = session.createQuery("from Profesor", Profesor.class).list();
-        for (Profesor profesor : result) {
-            System.out.println(profesor.getNombre() + " " + profesor.getApellidos());
-        }
-        session.getTransaction().commit();
-        session.close();
-    }
+	private static SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+
+	public void altaProfesor(Profesor profesor) {
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.save(profesor);
+		session.getTransaction().commit();
+		session.close();
+	}
+
+	public void consultarProfesores() {
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		List<Profesor> result = session.createQuery("from Profesor", Profesor.class).list();
+		for (Profesor profesor : result) {
+			System.out.println(profesor.getNombre() + " " + profesor.getApellidos());
+		}
+		session.getTransaction().commit();
+		session.close();
+	}
 }
